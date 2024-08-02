9b17216e838c16a8ac75cc83c36730ad71a59d89
DonutDev <58142952+DonutDev@users.noreply.github.com>
2024-08-02 12:14:09 +0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -2,131 +2,129 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
         id: 'sendimagetodiscord',
         title: "Send Image To Discord",
-        contexts:["image"],
+        contexts: ["image"],
     });
-    
+
     chrome.contextMenus.create({
         id: 'sendvideotodiscord',
         title: "Send Video To Discord",
-        contexts:["video"],
+        contexts: ["video"],
     });
-    
+
     chrome.contextMenus.create({
         id: 'sendaudiodiscord',
         title: "Send Audio To Discord",
-        contexts:["audio"],
+        contexts: ["audio"],
     });
-    
+
     chrome.contextMenus.create({
         id: 'sendselectiontodiscord',
         title: "Send Selection To Discord",
-        contexts:["selection"],
+        contexts: ["selection"],
     });
-    
+
     chrome.contextMenus.create({
         id: 'sendlinktodiscord',
         title: "Send Link To Discord",
-        contexts:["link"],
+        contexts: ["link"],
     });
-    
+
     chrome.contextMenus.create({
         id: 'sendpagetodiscord',
         title: "Send Page To Discord",
-        contexts:["page"],
+        contexts: ["page"],
     });
 });
 
-var webHookUrl = "";
-var username = "";
+let webHookUrl = "";
+let username = "";
 
-var webhook_is_valid = true;
+let webhook_is_valid = true;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     sendResponse({});
-    if (request.type == 'set_webhook_valid') {
+    if (request.type === 'set_webhook_valid') {
         webhook_is_valid = request.valid;
         console.log(request, webhook_is_valid);
-    }
-});
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == "sendimagetodiscord") {
-        sendimagetodiscord(info);
-    } else if (info.menuItemId == "sendvideotodiscord") {
-        sendvideotodiscord(info);
-    } else if (info.menuItemId == "sendaudiotodiscord") {
-        sendaudiotodiscord(info);
-    } else if (info.menuItemId == "sendselectiontodiscord") {
-        sendselectiontodiscord(info);
-    } else if (info.menuItemId == "sendlinktodiscord") {
-        sendlinktodiscord(info);
-    } else if (info.menuItemId == "sendpagetodiscord") {
-        sendpagetodiscord(info);
-    }
-});
-
-function sendimagetodiscord(info) {
-    postToWebhook("**" + username + "**: " + info.srcUrl);
-}
-
-function sendvideotodiscord(info) {
-    postToWebhook("**" + username + "**: " + info.srcUrl);
-}
-
-function sendaudiotodiscord(info) {
-    postToWebhook("**" + username + "**: " + info.srcUrl);
-}
-
-function sendselectiontodiscord(info) {
-    postToWebhook("**" + username + "**: \"*" + info.selectionText + "*\" - " + info.pageUrl);
-}
-
-function sendlinktodiscord(info) {
-    postToWebhook("**" + username + "**: " + info.linkUrl);
-}
-
-function sendpagetodiscord(info) {
-    postToWebhook("**" + username + "**: " + info.pageUrl);
-}
-
-function postToWebhook(content) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', webHookUrl, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-  
-    data = {
-        content: content,
-    }
-
-    xhr.send(JSON.stringify(data));
-
-    xhr.onload = function(res) {
-        console.log('posted: ', res);
-    }
-
-    xhr.onerror = function(res) {
-        console.log('error posting: ', res);
-    }
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    sendResponse({});
-    if (request.type == 'update') {
+    } else if (request.type === 'update') {
         refreshVars();
     }
 });
 
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    switch (info.menuItemId) {
+        case "sendimagetodiscord":
+            sendimagetodiscord(info);
+            break;
+        case "sendvideotodiscord":
+            sendvideotodiscord(info);
+            break;
+        case "sendaudiotodiscord":
+            sendaudiotodiscord(info);
+            break;
+        case "sendselectiontodiscord":
+            sendselectiontodiscord(info);
+            break;
+        case "sendlinktodiscord":
+            sendlinktodiscord(info);
+            break;
+        case "sendpagetodiscord":
+            sendpagetodiscord(info);
+            break;
+    }
+});
+
+async function postToWebhook(content) {
+    try {
+        const response = await fetch(webHookUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({ content: content })
+        });
+
+        if (response.ok) {
+            console.log('Posted to Discord');
+        } else {
+            console.error('Failed to post to Discord', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error('Error posting to Discord', error);
+    }
+}
+
+function sendimagetodiscord(info) {
+    postToWebhook(`**${username}**: ${info.srcUrl}`);
+}
+
+function sendvideotodiscord(info) {
+    postToWebhook(`**${username}**: ${info.srcUrl}`);
+}
+
+function sendaudiotodiscord(info) {
+    postToWebhook(`**${username}**: ${info.srcUrl}`);
+}
+
+function sendselectiontodiscord(info) {
+    postToWebhook(`**${username}**: "*${info.selectionText}*" - ${info.pageUrl}`);
+}
+
+function sendlinktodiscord(info) {
+    postToWebhook(`**${username}**: ${info.linkUrl}`);
+}
+
+function sendpagetodiscord(info) {
+    postToWebhook(`**${username}**: ${info.pageUrl}`);
+}
+
 function refreshVars() {
-    chrome.storage.sync.get('username', function(data) {
-        username = data.username;
-        console.log("username: " + username);
-    });
-    chrome.storage.sync.get('webHookUrl', function(data) {
-        webHookUrl = data.webHookUrl;
-        console.log("url: " + webHookUrl);
+    chrome.storage.sync.get(['username', 'webHookUrl'], function(data) {
+        username = data.username || '';
+        webHookUrl = data.webHookUrl || '';
+        console.log("username:", username);
+        console.log("url:", webHookUrl);
     });
 }
 
 refreshVars();
-
-console.log(chrome.identity.getRedirectURL());
